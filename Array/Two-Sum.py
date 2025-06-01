e316a47a2205b8bc69e43149f97d6200042b7d0d
HuuPhuocBui <135402077+HuuPhuocBui@users.noreply.github.com>
2025-06-01 20:55:38 +0700

    1/6/2025

---
+++ Array/Two-Sum.py
@@ -0,0 +1,17 @@
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in seen:
+                return [seen[diff], i]
+            else:
+                seen[nums[i]] = i
+
+sol = Solution()
+print(sol.twoSum([2, 7, 11, 15], 9))
